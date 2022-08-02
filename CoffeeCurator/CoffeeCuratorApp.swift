ba5a77d52537c6ccc4bc1d0522a307e9ad8234ed
Asel Zh <assollzh@gmail.com>
2022-08-02 10:47:24 -0700

    Initial commit

---
+++ CoffeeCurator/CoffeeCuratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeCuratorApp.swift
+//  CoffeeCurator
+//
+//  Created by Viktoriya Kudryashova on 8/2/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeCuratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
